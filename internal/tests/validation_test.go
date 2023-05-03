303cc051769a99343754b5bd86881546eec241f1
Maxim Golovin <93408191+InfinityMeta@users.noreply.github.com>
2023-05-03 23:00:40 +0300

    Add files via upload

---
+++ internal/tests/validation_test.go
@@ -0,0 +1,100 @@
+package tests
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAd_EmptyTitle(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	_, err := client.createAd(0, "", "world")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestCreateAd_TooLongTitle(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	title := strings.Repeat("a", 101)
+
+	_, err := client.createAd(0, title, "world")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestCreateAd_EmptyText(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	_, err := client.createAd(0, "title", "")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestCreateAd_TooLongText(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	text := strings.Repeat("a", 501)
+
+	_, err := client.createAd(0, "title", text)
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestUpdateAd_EmptyTitle(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	resp, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	_, err = client.updateAd(0, resp.Data.ID, "", "new_world")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestUpdateAd_TooLongTitle(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	resp, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	title := strings.Repeat("a", 101)
+
+	_, err = client.updateAd(0, resp.Data.ID, title, "world")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestUpdateAd_EmptyText(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	resp, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	_, err = client.updateAd(0, resp.Data.ID, "title", "")
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
+
+func TestUpdateAd_TooLongText(t *testing.T) {
+	client := getTestClient()
+
+	_, _ = client.createUser("Bob", "bob@box.com")
+
+	text := strings.Repeat("a", 501)
+
+	resp, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	_, err = client.updateAd(0, resp.Data.ID, "title", text)
+	assert.ErrorIs(t, err, ErrBadRequest)
+}
